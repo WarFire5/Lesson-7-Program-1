aa635857c8d89b0294793d42325146efbe7b0bf9
Татьяна <@WIN-E362SIULNK8>
2023-02-03 21:40:09 +0300

    Добавьте файлы проекта.

---
+++ Lesson 7 Program 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7_Program_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пароль:");
+            string password = Console.ReadLine();
+                        
+            string entrance = "Доступ разрешён";
+
+            string forbidden = "Доступ запрещён";
+
+            if (password == "QWERTY123")
+            {
+                Console.WriteLine(entrance);
+            }                                 
+
+            if (password != "QWERTY123")
+            {
+                Console.WriteLine(forbidden);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
